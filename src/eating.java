dc399ab8eec33530689fef59dab264a04c227676
Christine Nixon <cnixon11@Christines-MacBook-Air.local>
2019-05-30 11:57:51 -0400

    deleted eventType

--- src/eating.java
+++ src/eating.java
@@ -10,7 +10,9 @@ public class eating {
 
 		System.out.println("Is this a casual, semi-formal, or formal event?");
 		String eventType = scanner.next();
-		eventType = "casual";
+
+		System.out.println("How many people are in your party?");
+		int partySize = scanner.nextInt();
 
 		if (eventType == "casual") {
 			mealSuggestion = "sandwiches";
@@ -20,9 +22,6 @@ public class eating {
 			mealSuggestion = "chicken parmesan";
 		}
 
-		System.out.println("How many people are in your party?");
-		int partySize = scanner.nextInt();
-
 		if (partySize == 1) {
 			prepSuggestion = "in the microwave";
 		} else if (partySize >= 2 && partySize <= 12) {
@@ -30,13 +29,13 @@ public class eating {
 		} else if (partySize >= 13) {
 			prepSuggestion = "by a caterer";
 		}
+
+		String result;
 		System.out.print("Since you are hosting a " + eventType);
 		System.out.print(" event for " + partySize);
 		System.out.print(" participants, you should serve " + mealSuggestion);
 
 		System.out.print(" prepared " + prepSuggestion + ".");
-
-		String result;
 
 	}
 
